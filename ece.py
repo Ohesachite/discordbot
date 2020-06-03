3d228a293cb91f101ad66eff6c330b1cd34643a4
Ohesachite <Alan2001liu09@yahoo.com>
2020-06-03 18:41:36 -0400

    Resolved hierachy issue

--- ece.py
+++ ece.py
@@ -6,37 +6,36 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot = commands.Bot(command_prefix='stink ')
-TOKEN = os.environ.get('')
+os.environ.get('')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+    await bot.change_presence(activity=discord.Game("Stinking the ECE Building"))
 
 @bot.command(name='ece', help='Stinky ECE majors!')
 async def uh_oh(ctx):
     await ctx.send('Uh oh! Stinky!')
 
-@bot.command(name='Ece', help='Same as ece')
+@bot.command(name='Ece', help='Stinky ECE majors!')
 async def uh_oh_cap(ctx):
     await ctx.send('Uh oh! Stinky!')
 
-@bot.command(name='ECE', help='Same as ece')
+@bot.command(name='ECE', help='Stinky ECE majors!')
 async def uh_oh_upper(ctx):
     await ctx.send('Uh oh! Stinky!')
 
 @bot.command(name='add', help='Add group chat for class')
 async def add_class(ctx, subject, course_number):
     user = ctx.message.author
-    print('Reached fetching phase')
     role = discord.utils.get(user.guild.roles, name = subject.upper() + ' ' + course_number)
     if(role is None):
         await ctx.send('Not a class on this server!')
     else:
-        print('Reached adding phase')
         await user.add_roles(role)
-        print('Done')
+        await ctx.send('Class added!')
 
-@add_class.error
+@bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.MissingRequiredArgument):
         await ctx.send('Not enough parameters!')
